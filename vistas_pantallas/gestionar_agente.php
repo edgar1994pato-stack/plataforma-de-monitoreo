fcce743d87aa3123a6048ec5d0400ef449037ffc
EDGAR PATRICIO CHILLAGANA BOMBON <echillaganab@ad.alfanet.net.ec>
2026-03-27 01:11:21 -0500

    se actualiza no se

--- vistas_pantallas/gestionar_agente.php
+++ vistas_pantallas/gestionar_agente.php
@@ -5,6 +5,7 @@ require_once BASE_PATH . '/includes_partes_fijas/seguridad.php';
 
 require_login();
 force_password_change();
+require_permission('ver_modulo_agentes');
 
 function h($str){ return htmlspecialchars((string)$str, ENT_QUOTES, 'UTF-8'); }
 
