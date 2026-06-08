a59d3ff66557d9c2bec28ad6782e13d78ed5da20
EDGAR PATRICIO CHILLAGANA BOMBON <echillaganab@ad.alfanet.net.ec>
2026-06-07 19:54:43 -0500

    Corrige cálculo de nota para área GXC

--- vistas_pantallas/formulario.php
+++ vistas_pantallas/formulario.php
@@ -1079,7 +1079,7 @@ function recalcularScoreEnVivo(){
       - Otras áreas:
           Nota = 100 - puntos_fallados
     */
-     if (idAreaActual === 3 || idAreaActual === 6 || idAreaActual === 7 || idAreaActual === 2 || idAreaActual === 1 ) {
+     if (idAreaActual === 3 || idAreaActual === 6 || idAreaActual === 7 || idAreaActual === 2 || idAreaActual === 1 || idAreaActual === 8 ) {
       nota = puntosPosibles > 0
         ? (puntosObtenidos / puntosPosibles) * 100
         : 0;
